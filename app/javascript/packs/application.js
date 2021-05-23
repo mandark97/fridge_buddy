dda05ed7b325658f8d753a34a0b35b06e8a3c5ac
Matei Melinte <matei_melinte@epam.com>
2021-05-23 23:53:49 +0300

    assets fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import 'bootstrap'
+import './application.scss'
 import "channels"
 
 Rails.start()
